cc8b91e31eedc4c7b2d2a190a302770593962596
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2017-09-28 11:12:09 +0300

    ui label added and uilabel button added

--- Kugim Project 1/ViewController.swift
+++ Kugim Project 1/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
 
+    @IBAction func changeUILabelButton() {
+        nameLabel.text = "UI Label Changed"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // nameLabel.text = "Name Input"
     }
 
     override func didReceiveMemoryWarning() {
